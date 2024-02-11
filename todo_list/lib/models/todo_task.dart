f6c37e67993d4ee6238866fa9d70497bf247e37a
losikilove <75561119+losikilove@users.noreply.github.com>
2024-02-11 15:42:34 +0700

    Update todo_task.dart

--- todo_list/lib/models/todo_task.dart
+++ todo_list/lib/models/todo_task.dart
@@ -3,10 +3,12 @@ class TodoTask {
   final int id;
   final String title;
   final DateTime duration;
+  bool _isDone;
 
   TodoTask(title, duration)
       : title = title,
         duration = duration,
+        _isDone = false,
         id = _countId++;
 
   String get showDate =>
@@ -17,4 +19,10 @@ class TodoTask {
 
   String get showDuration =>
       '${duration.year}/${duration.month.toString().padLeft(2, '0')}/${duration.day.toString().padLeft(2, '0')} ${duration.hour.toString().padLeft(2, '0')}:${duration.minute.toString().padLeft(2, '0')}';
+
+  bool get isDone => _isDone;
+
+  set setIsDone(bool value) {
+    _isDone = value;
+  }
 }
